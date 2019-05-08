8f9e1a1f15e793f69263c288ce254f6b997a52f5
Vincent <vincent.durufle@hotmail.fr>
2019-05-08 13:22:40 +0200

    script change

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -3,10 +3,10 @@
         arrows.map(arrow => {
             arrow.addEventListener('click', function () {
                 console.dir(backgroundImage);
-                if (backgroundImage.src === "http://127.0.0.1:5500/ressources/img/slider/bg1.jpg") {
-                    backgroundImage.src = "http://127.0.0.1:5500/ressources/img/slider/bg2.jpg";
+                if (backgroundImage.src === "https://quizzical-easley-1712a9.netlify.com/ressources/img/slider/bg1.jpg") {
+                    backgroundImage.src = "https://quizzical-easley-1712a9.netlify.com/ressources/img/slider/bg2.jpg";
                 } else {
-                    backgroundImage.src = "http://127.0.0.1:5500/ressources/img/slider/bg1.jpg";
+                    backgroundImage.src = "https://quizzical-easley-1712a9.netlify.com/ressources/img/slider/bg1.jpg";
                 }
             })
         })
